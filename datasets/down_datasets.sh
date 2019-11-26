551e2ef3e8f1a6e7f43f0dcc0deb4ce533ee12d7
ForawardStar <32625467+ForawardStar@users.noreply.github.com>
2019-11-26 11:42:18 +0800

    Update down_datasets.sh

--- datasets/down_datasets.sh
+++ datasets/down_datasets.sh
@@ -16,6 +16,3 @@ URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
-mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./datasets/
-rm $ZIP_FILE
