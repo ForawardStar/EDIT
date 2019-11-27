23bf89a54b6021e9b1a3b30a1d0886e4d18de568
ForawardStar <32625467+ForawardStar@users.noreply.github.com>
2019-11-27 21:25:10 +0800

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='epoch to start training from')
-parser.add_argument('--n_epochs', type=int, default=3000, help='number of epochs of training')
+parser.add_argument('--n_epochs', type=int, default=300, help='number of epochs of training')
 parser.add_argument('--dataset_name', type=str, default="monet2photo", help='name of the dataset')
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--lr', type=float, default=0.0001, help='adam: learning rate')
